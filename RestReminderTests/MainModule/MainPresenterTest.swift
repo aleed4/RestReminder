9306f38cdcc139491a4f967911aa735ed65690f7
aleed4 <work.aleed@gmail.com>
2022-10-27 13:15:25 +0300

    12

---
+++ RestReminderTests/MainModule/MainPresenterTest.swift
@@ -0,0 +1,31 @@
+//
+//  MainPresenterTest.swift
+//  RestReminderTests
+//
+//  Created by Александр Лебедев on 27.10.2022.
+//
+
+import XCTest
+
+final class MainPresenterTest: XCTestCase {
+
+    override func setUpWithError() throws {
+       
+    }
+
+    override func tearDownWithError() throws {
+       
+    }
+
+    func testExample() throws {
+       
+    }
+
+    func testPerformanceExample() throws {
+      
+        self.measure {
+           
+        }
+    }
+
+}
